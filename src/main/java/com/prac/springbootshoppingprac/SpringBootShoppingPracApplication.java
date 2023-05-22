7ca2a927f2be2858d42fcdd5e161e3ef7e56fab2
eastmeet <ys932184@gmail.com>
2023-05-22 17:46:44 +0900

    first commit

---
+++ src/main/java/com/prac/springbootshoppingprac/SpringBootShoppingPracApplication.java
@@ -0,0 +1,13 @@
+package com.prac.springbootshoppingprac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootShoppingPracApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootShoppingPracApplication.class, args);
+    }
+
+}
